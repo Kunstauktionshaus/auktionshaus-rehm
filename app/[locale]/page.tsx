3331d0bdcd2b13bcf93dab25bb0a0667c945cb94
Nazaranka Yuliya <nazarenko.ylia@gmail.com>
2024-10-23 20:14:45 +0200

    try external image link

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,22 @@
+import Image from "next/image";
 const Home = () => {
-  return <div>Home</div>;
+  const nr = 312;
+  const auc = 1001;
+
+  const imageUrl = `https://vigorous-satoshi.87-106-178-189.plesk.page/images/${nr}_${auc}.png`;
+
+  return (
+    <div>
+      <Image
+        src={imageUrl}
+        alt="image of item"
+        width={500}
+        height={500}
+        className="cursor-pointer object-cover"
+        quality={100}
+      />
+    </div>
+  );
 };
 
 export default Home;
--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,20 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
-  output: "standalone",
+  images: {
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "vigorous-satoshi.87-106-178-189.plesk.page",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
+  },
 };
 
 export default withNextIntl(nextConfig);
